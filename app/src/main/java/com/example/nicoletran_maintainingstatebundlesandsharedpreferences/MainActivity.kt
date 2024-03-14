0ea6fc982e80fb20853016de041d74d1d00f1427
Nicole Tran <nktran3@bu.edu>
2024-03-14 18:43:33 -0400

    add images and set button on click to load random images

--- app/src/main/java/com/example/nicoletran_maintainingstatebundlesandsharedpreferences/MainActivity.kt
+++ app/src/main/java/com/example/nicoletran_maintainingstatebundlesandsharedpreferences/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.nicoletran_maintainingstatebundlesandsharedpreferences
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var imageView: ImageView
+    private val images = listOf(R.drawable.puppies1, R.drawable.puppies2, R.drawable.puppies3, R.drawable.puppies4, R.drawable.puppies5)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageView = findViewById(R.id.image_view)
+        val button: Button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            val randomImage = images.random()
+            imageView.setImageResource(randomImage)
+        }
     }
 }
