5520a80e472c1c1f39d699a155952d129f63ca95
Nicole Tran <nktran3@bu.edu>
2024-03-14 23:57:17 -0400

    saved preferences

--- app/src/main/java/com/example/nicoletran_maintainingstatebundlesandsharedpreferences/MainActivity.kt
+++ app/src/main/java/com/example/nicoletran_maintainingstatebundlesandsharedpreferences/MainActivity.kt
@@ -6,17 +6,33 @@ import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var imageView: ImageView
+    private lateinit var editText: EditText
     private val images = listOf(R.drawable.puppies1, R.drawable.puppies2, R.drawable.puppies3, R.drawable.puppies4, R.drawable.puppies5)
+    private var savedImage: Int = images.first()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         imageView = findViewById(R.id.image_view)
+        editText = findViewById(R.id.edit_text)
         val button: Button = findViewById(R.id.button)
 
+        val sharedPreferences = getSharedPreferences("sharedPreferences", MODE_PRIVATE)
+        savedImage = sharedPreferences.getInt("image", images.first())
+        val savedText = sharedPreferences.getString("text", "")
+        imageView.setImageResource(savedImage)
+        editText.setText(savedText)
+
         button.setOnClickListener {
-            val randomImage = images.random()
-            imageView.setImageResource(randomImage)
+            savedImage = images.random()
+            imageView.setImageResource(savedImage)
         }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        val sharedPreferences = getSharedPreferences("sharedPreferences", MODE_PRIVATE).edit()
+        sharedPreferences.putInt("image", savedImage)
+        sharedPreferences.putString("text", editText.text.toString())
+        sharedPreferences.apply()
     }
 }
